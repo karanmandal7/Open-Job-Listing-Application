bd21a59d86727799c730b954ebe4802cfd17a6c2
karan1171 <karanm98033@gmail.com>
2025-03-31 14:15:24 +0530

    baseUrl Updated

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
 export const axiosClient = axios.create({
-    baseURL:'http://localhost:1234/api/v1'
+   // baseURL:'http://localhost:1234/api/v1'
+   baseURL:'https://open-job-listing-application.onrender.com/api/v1'
 })
